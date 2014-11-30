7475077255476417fb42b1439d657c72d07c014d
bee_keeper <ricbottomley@gmail.com>
2014-11-30 00:24:31 +0000

    template path

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ from setuptools import setup, find_packages
 setup(
   name = 'django-invitations',
   packages=find_packages(),
-  version = '0.1',
+  version = '0.2',
   description = 'Django invitation integration for django-allauth',
   author = 'https://github.com/bee-keeper',
   author_email = 'none@none.com',
   url = 'https://github.com/bee-keeper/django-invitations.git',
-  download_url = 'https://github.com/bee-keeper/django-invitations/tarball/0.1',
+  download_url = 'https://github.com/bee-keeper/django-invitations/tarball/0.2',
   keywords = ['django', 'invitation', 'django-allauth', 'invite'],
   classifiers = [],
   install_requires=[
