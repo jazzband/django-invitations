ee00e1d9894c67b5466170e5fea0f263d4174388
Nic <nicholasdwoodward@gmail.com>
2015-08-03 04:09:01 -0400

    Admin invitations
    sends invitations from admin on save

--- invitations/admin.py
+++ invitations/admin.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
 
 from .models import Invitation
-
+from .forms import InviteModelForm
 
 class InvitationAdmin(admin.ModelAdmin):
     list_display = ('email', 'sent', 'accepted')
-    model = Invitation
+    form = InviteModelForm
+
+    def get_form(self, request, obj=None, **kwargs):
+		form = super(InvitationAdmin, self).get_form(request, **kwargs)
+		form.user = request.user
+		form.request = request
+		return form
 
 admin.site.register(Invitation, InvitationAdmin)
