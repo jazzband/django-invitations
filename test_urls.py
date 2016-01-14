cb4626e0174fed6d86deabaa37e7fa66ad84026a
bee_keeper <ricbottomley@gmail.com>
2016-01-14 11:58:31 +0000

    test new url spec

--- test_urls.py
+++ test_urls.py
@@ -1,18 +1,16 @@
-from django.conf.urls import patterns, url, include
+from django.conf.urls import url, include
 from django.contrib import admin
 from django.conf import settings
 
 admin.autodiscover()
 
-urlpatterns = patterns(
-    '',
+urlpatterns = [
     url(r'^invitations/', include(
         'invitations.urls', namespace='invitations')),
     url(r'^admin/', include(admin.site.urls)),
-)
+]
 
 if 'allauth' in settings.INSTALLED_APPS:
-    urlpatterns += patterns(
-        '',
+    urlpatterns.append(
         url(r'^accounts/', include('allauth.urls'))
     )
