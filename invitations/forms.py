ae2dd48456eb0eb13798a504459b09df6879019c
bee-keeper <ricbottomley@gmail.com>
2016-12-15 12:48:49 +0000

    flake8

--- invitations/forms.py
+++ invitations/forms.py
@@ -48,9 +48,8 @@ class InviteForm(forms.Form, CleanEmailMixin):
     email = forms.EmailField(
         label=_("E-mail"),
         required=True,
-        widget=forms.TextInput(attrs={"type": "email", "size": "30"}, ),
-                               initial=""
-                              )
+        widget=forms.TextInput(
+            attrs={"type": "email", "size": "30"}), initial="")
 
     def save(self, email):
         return Invitation.create(email=email)
