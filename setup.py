c7ee5f543274c8790215966631a1e3314d95ec60
bee-keeper <ricbottomley@gmail.com>
2017-02-11 10:47:31 +0000

    1.9 release

--- setup.py
+++ setup.py
@@ -6,13 +6,13 @@ setup(
     package_data={'invitations': ['templates/*.*']},
     include_package_data=True,
     zip_safe=False,
-    version='1.8',
+    version='1.9',
     description='Generic invitations app with support for django-allauth',
     author='https://github.com/bee-keeper',
     author_email='none@none.com',
     url='https://github.com/bee-keeper/django-invitations.git',
     download_url='https://github.com/'
-                 'bee-keeper/django-invitations/tarball/1.8',
+                 'bee-keeper/django-invitations/tarball/1.9',
     keywords=['django', 'invitation', 'django-allauth', 'invite'],
     classifiers=[
         'Development Status :: 4 - Beta',
