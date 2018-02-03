0f4ea68c8aaff0d70f1ef01d84f7f5e24cfed0c2
bee-keeper <ricbottomley@gmail.com>
2018-02-03 14:55:05 +0000

    settings

--- test_settings.py
+++ test_settings.py
@@ -41,10 +41,11 @@ else:
     )
 
 MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
