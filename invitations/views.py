8a95be93caf5c1cbadab4e3c752acccbc230d4c4
Aaron Kazah <aaronkazah@gmail.com>
2018-12-16 15:50:03 +0000

    Updated formatting to comply with PEP8 standards

--- invitations/views.py
+++ invitations/views.py
@@ -178,7 +178,10 @@ def accept_invitation(invitation, request, signal_sender):
     invitation.accepted = True
     invitation.save()
 
-    invite_accepted.send(sender=signal_sender, email=invitation.email, request=request)
+    invite_accepted.send(
+        sender=signal_sender, 
+        email=invitation.email, 
+        request=request)
 
     get_invitations_adapter().add_message(
         request,
