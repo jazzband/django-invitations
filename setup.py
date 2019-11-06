a312fe7815bb828fb973ad6ae9f115edebfd8ee6
Greg Twohig <greg@deepvantage.com>
2019-11-06 18:13:57 +0200

    Updates testing to test current versions and pass
    Version support per:
    - https://devguide.python.org/#status-of-python-branches
    - https://docs.djangoproject.com/en/2.2/faq/install/#what-python-version-should-i-use-with-django
    
    Gets passing tests by marking the one failing test as requiring the database (which it does)
    
    updates requirements.txt to current requirement version and adds tox and flake8 to requirements for local development.

--- setup.py
+++ setup.py
@@ -22,9 +22,10 @@ setup(
         'Topic :: Internet',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Framework :: Django',
     ],
 )
