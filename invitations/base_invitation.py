4eff4c587a2a059c65b7961571363d0d244b27d0
Francis Secada <francis.secada@gmail.com>
2019-12-26 14:32:54 -0500

    Added six module for python_2_unicode_compatible functionality. Should be compatible with Django 3.0

--- invitations/base_invitation.py
+++ invitations/base_invitation.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.db import models
-from django.utils.encoding import python_2_unicode_compatible
+# from django.utils.encoding import python_2_unicode_compatible
+from six import python_2_unicode_compatible
 from django.utils.translation import ugettext_lazy as _
 
 from .managers import BaseInvitationManager
