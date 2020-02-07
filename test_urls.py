b0fff71ba7db8331556eac3499c42995e466dd19
bee-keeper <ricbottomley@gmail.com>
2020-02-07 17:09:04 +0000

    continue to test against 1.x

--- test_urls.py
+++ test_urls.py
@@ -1,15 +1,15 @@
-from django.urls import re_path, include
+from django.conf.urls import url, include
 from django.contrib import admin
 from django.conf import settings
 
 admin.autodiscover()
 
 urlpatterns = [
-    re_path(r'^invitations/', include('invitations.urls')),
-    re_path(r'^admin/', admin.site.urls),
+    url(r'^invitations/', include('invitations.urls')),
+    url(r'^admin/', admin.site.urls),
 ]
 
 if 'allauth' in settings.INSTALLED_APPS:
     urlpatterns.append(
-        re_path(r'^accounts/', include('allauth.urls'))
+        url(r'^accounts/', include('allauth.urls'))
     )
