15d1b3332127a7877622aac70bae4d42919eff5f
Víðir Valberg Guðmundsson <valberg@orn.li>
2022-03-30 18:02:23 +0200

    Ignore migrations when doing pre-commit.

--- invitations/migrations/0001_initial.py
+++ invitations/migrations/0001_initial.py
@@ -7,47 +7,22 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
 
-    dependencies = []
+    dependencies = [
+    ]
 
     operations = [
         migrations.CreateModel(
-            name="Invitation",
+            name='Invitation',
             fields=[
-                (
-                    "id",
-                    models.AutoField(
-                        verbose_name="ID",
-                        serialize=False,
-                        auto_created=True,
-                        primary_key=True,
-                    ),
-                ),
-                (
-                    "email",
-                    models.EmailField(
-                        unique=True,
-                        max_length=75,
-                        verbose_name="e-mail address",
-                    ),
-                ),
-                (
-                    "accepted",
-                    models.BooleanField(default=False, verbose_name="accepted"),
-                ),
-                (
-                    "created",
-                    models.DateTimeField(
-                        default=django.utils.timezone.now,
-                        verbose_name="created",
-                    ),
-                ),
-                (
-                    "key",
-                    models.CharField(unique=True, max_length=64, verbose_name="key"),
-                ),
-                ("sent", models.DateTimeField(null=True, verbose_name="sent")),
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('email', models.EmailField(unique=True, max_length=75, verbose_name='e-mail address')),
+                ('accepted', models.BooleanField(default=False, verbose_name='accepted')),
+                ('created', models.DateTimeField(default=django.utils.timezone.now, verbose_name='created')),
+                ('key', models.CharField(unique=True, max_length=64, verbose_name='key')),
+                ('sent', models.DateTimeField(null=True, verbose_name='sent')),
             ],
-            options={},
+            options={
+            },
             bases=(models.Model,),
         ),
     ]
--- invitations/migrations/0002_auto_20151126_0426.py
+++ invitations/migrations/0002_auto_20151126_0426.py
@@ -5,33 +5,26 @@ import django.db.models.deletion
 from django.conf import settings
 from django.db import migrations, models
 
-EMAIL_MAX_LENGTH = getattr(settings, "INVITATIONS_EMAIL_MAX_LENGTH", 254)
+EMAIL_MAX_LENGTH = getattr(settings, 'INVITATIONS_EMAIL_MAX_LENGTH', 254)
+
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ("invitations", "0001_initial"),
+        ('invitations', '0001_initial'),
     ]
 
     operations = [
         migrations.AddField(
-            model_name="invitation",
-            name="inviter",
-            field=models.ForeignKey(
-                to=settings.AUTH_USER_MODEL,
-                null=True,
-                on_delete=django.db.models.deletion.CASCADE,
-            ),
+            model_name='invitation',
+            name='inviter',
+            field=models.ForeignKey(to=settings.AUTH_USER_MODEL, null=True, on_delete=django.db.models.deletion.CASCADE),
         ),
         migrations.AlterField(
-            model_name="invitation",
-            name="email",
-            field=models.EmailField(
-                unique=True,
-                max_length=EMAIL_MAX_LENGTH,
-                verbose_name="e-mail address",
-            ),
+            model_name='invitation',
+            name='email',
+            field=models.EmailField(unique=True, max_length=EMAIL_MAX_LENGTH, verbose_name='e-mail address'),
         ),
     ]
