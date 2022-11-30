cb983f7d5155506b57ee85f12f18be29eb7eddbf
ErikDeskBox <erikand@gmail.com>
2022-11-30 01:55:47 +0100

    Set default primary key, no migration needed
    Fixes issue #205

--- invitations/apps.py
+++ invitations/apps.py
@@ -4,5 +4,6 @@ from django.apps import AppConfig
 class Config(AppConfig):
     """Config."""
 
+    default_auto_field = "django.db.models.AutoField"
     name = "invitations"
     label = "invitations"
