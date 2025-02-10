65a531cebd27643013a3f117e1f67e39cfe6100e
demestav <1180929+demestav@users.noreply.github.com>
2025-02-10 15:48:00 +0200

    Added resend action to admin

--- invitations/admin.py
+++ invitations/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
 
 from .utils import (
     get_invitation_admin_add_form,
@@ -15,6 +16,7 @@ InvitationAdminChangeForm = get_invitation_admin_change_form()
 class InvitationAdmin(admin.ModelAdmin):
     list_display = ("email", "sent", "accepted")
     autocomplete_fields = ["inviter"]
+    actions = ["resend_invitation"]
 
     def get_form(self, request, obj=None, **kwargs):
         if obj:
@@ -24,3 +26,9 @@ class InvitationAdmin(admin.ModelAdmin):
             kwargs["form"].user = request.user
             kwargs["form"].request = request
         return super().get_form(request, obj, **kwargs)
+
+    @admin.action(description=_("Resend invitation"))
+    def resend_invitation(self, request, queryset):
+        for invitation in queryset:
+            invitation.send_invitation(request)
+        self.message_user(request, _("Selected invitations have been resent."))
